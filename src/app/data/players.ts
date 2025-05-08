5e3fa6b88aaf60b203f1842b181209474bedae3c
RahatKarim1795 <rht.krt@gmail.com>
2025-05-08 22:07:58 +0600

    players inputted

--- src/app/data/players.ts
+++ src/app/data/players.ts
@@ -4,91 +4,151 @@ export const players: Player[] = [
   {
     id: '1',
     name: 'Abrar',
-    positions: ['Midfielder','Forward'],
+    positions: ['Midfielder', 'Forward'],
     rating: 9.0
   },
   {
     id: '2',
-    name: 'Alvi',
+    name: 'Alvi wasifur',
     positions: ['Forward'],
     rating: 9.0
   },
   {
     id: '3',
-    name: 'Jabir',
+    name: 'Faiyad',
     positions: ['Goalkeeper'],
-    rating: 7.0
+    rating: 8.5
   },
   {
     id: '4',
-    name: 'Hassan',
-    positions: ['Defender'],
-    rating: 7
+    name: 'Rafa',
+    positions: ['Defender', 'Forward', 'Midfielder'],
+    rating: 7.5
   },
   {
     id: '5',
-    name: 'Ibrahim',
-    positions: ['Midfielder'],
-    rating: 9
+    name: 'Ishan',
+    positions: ['Defender'],
+    rating: 7.0
   },
   {
     id: '6',
-    name: 'Karim',
-    positions: ['Midfielder'],
-    rating: 8
+    name: 'Jawad',
+    positions: ['Defender'],
+    rating: 6.5
   },
   {
     id: '7',
-    name: 'Mahmoud',
-    positions: ['Midfielder', 'Forward'],
-    rating: 7
+    name: 'Mighty',
+    positions: ['Forward'],
+    rating: 8.0
   },
   {
     id: '8',
-    name: 'Omar',
-    positions: ['Forward'],
-    rating: 9
+    name: 'Marzan',
+    positions: ['Defender'],
+    rating: 7.0
   },
   {
     id: '9',
-    name: 'Youssef',
-    positions: ['Forward'],
-    rating: 8
+    name: 'Morshed',
+    positions: ['Midfielder', 'Defender'],
+    rating: 7.5
   },
   {
     id: '10',
-    name: 'Ziad',
-    positions: ['Goalkeeper'],
-    rating: 6
+    name: 'Fida',
+    positions: ['Defender', 'Midfielder'],
+    rating: 6.5
   },
   {
     id: '11',
-    name: 'Khaled',
-    positions: ['Defender'],
-    rating: 8
+    name: 'Pranto',
+    positions: ['Forward', 'Midfielder'],
+    rating: 8.0
   },
   {
     id: '12',
-    name: 'Tarek',
-    positions: ['Defender', 'Midfielder'],
-    rating: 7
+    name: 'Qavi',
+    positions: ['Midfielder', 'Forward'],
+    rating: 7.5
   },
   {
     id: '13',
-    name: 'Samir',
+    name: 'Ratz Rahat',
     positions: ['Midfielder'],
-    rating: 6
+    rating: 8.0
   },
   {
     id: '14',
-    name: 'Walid',
-    positions: ['Midfielder', 'Forward'],
-    rating: 8
+    name: 'Rahat fusda',
+    positions: ['Defender', 'Midfielder', 'Goalkeeper'],
+    rating: 6.0
   },
   {
     id: '15',
-    name: 'Adel',
+    name: 'Rakib bhai',
+    positions: ['Forward', 'Midfielder'],
+    rating: 7.5
+  },
+  {
+    id: '16',
+    name: 'Rakib jr',
+    positions: ['Forward', 'Defender', 'Midfielder'],
+    rating: 7.0
+  },
+  {
+    id: '17',
+    name: 'Rimon',
+    positions: ['Defender'],
+    rating: 7.0
+  },
+  {
+    id: '18',
+    name: 'Rooman',
+    positions: ['Defender', 'Goalkeeper'],
+    rating: 6.5
+  },
+  {
+    id: '19',
+    name: 'Marshall',
+    positions: ['Goalkeeper'],
+    rating: 8.0
+  },
+  {
+    id: '20',
+    name: 'Shegz',
+    positions: ['Defender', 'Midfielder'],
+    rating: 7.5
+  },
+  {
+    id: '21',
+    name: 'Shoaib',
+    positions: ['Midfielder'],
+    rating: 7.0
+  },
+  {
+    id: '22',
+    name: 'Tamjid',
     positions: ['Forward'],
-    rating: 7
+    rating: 8.0
+  },
+  {
+    id: '23',
+    name: 'Tanjim',
+    positions: ['Forward', 'Midfielder'],
+    rating: 7.5
+  },
+  {
+    id: '24',
+    name: 'Wahid',
+    positions: ['Defender'],
+    rating: 6.5
+  },
+  {
+    id: '25',
+    name: 'Zamil',
+    positions: ['Forward', 'Midfielder', 'Defender'],
+    rating: 7.0
   }
 ]; 
